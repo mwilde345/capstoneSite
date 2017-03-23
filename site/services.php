2fda2394f5b5aa32c6a2eb1058aebea885718422
dallendalton <dallendalton@gmail.com>
2017-03-23 13:42:16 -0600

    Edited services page

--- site/services.php
+++ site/services.php
@@ -19,71 +19,54 @@ jQuery("body").attr('class','index-1');
 					<div class="grid_4">
 						<div class="img_block"><a href="images/index-1_img-1-big.jpg"><img src="images/index-1_img-1.jpg" alt="Services"><i>+</i></a></div>
 						<article>
-							<h4>Lorem Nave</h4>
-							<p>Gamus at magna non nunc tristique rhoncuseri tym. Aliquam nibh ante, egestas id dictum aterert commodo re luctus libero. Praesent faucibus malesuada cibuste. Donec laoreet metus id laoreet malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur orci sed Curabitur vel lorem sit amet nulla ullamcorper fermentum. In vitae varius augue, eu consectetur ligula. Etiam dui eros, laoreet sit amet est vel, commodo venenatis eros.</p>
+							<h4>Air & Water Balance</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+							<a class="button request_services">Request Service</a>
 						</article>
 					</div>
 					<div class="grid_4">
 						<div class="img_block"><a href="images/index-1_img-2-big.jpg"><img src="images/index-1_img-2.jpg" alt="Services"><i>+</i></a></div>
 						<article>
-							<h4>Dertomer eli</h4>
-							<p>Hamus at magna non nunc tristique rhoncuseri tym. Aliquam nibh ante, egestas id dictum aterert commodo re luctus libero. Praesent faucibus malesuada cibuste. Donec laoreet metus id laoreet malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur orci sed Curabitur vel lorem sit amet nulla ullamcorper fermentum. In vitae varius augue, eu consectetur ligula. Etiam dui eros, laoreet sit amet est vel, commodo venenatis erose.</p>
+							<h4>HVAC Systems Analysis</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+							<a class="button request_services">Request Service</a>
 						</article>
 					</div>
 					<div class="grid_4">
 						<div class="img_block"><a href="images/index-1_img-3-big.jpg"><img src="images/index-1_img-3.jpg" alt="Services"><i>+</i></a></div>
 						<article>
-							<h4>Sentro Merid</h4>
-							<p>Famus at magna non nunc tristique rhoncuseri tym. Aliquam nibh ante, egestas id dictum aterert commodo re luctus libero. Praesent faucibus malesuada cibuste. Donec laoreet metus id laoreet malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur orci sed Curabitur vel lorem sit amet nulla ullamcorper fermentum. In vitae varius augue, eu consectetur ligula. Etiam dui eros, laoreet sit amet est vel, commodo venenatis eros eto.</p>
+							<h4>HVAC Systems Design</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+							<a class="button request_services">Request Service</a>
 						</article>
 					</div>
 					<div class="grid_4">
 						<div class="img_block"><a href="images/index-1_img-4-big.jpg"><img src="images/index-1_img-4.jpg" alt="Services"><i>+</i></a></div>
 						<article>
-							<h4>Berto Mero</h4>
-							<p>Samus at magna non nunc tristique rhoncuseri tym. Aliquam nibh ante, egestas id dictum aterert commodo re luctus libero. Praesent faucibus malesuada cibuste. Donec laoreet metus id laoreet malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur orci sed Curabitur vel lorem sit amet nulla ullamcorper fermentum. In vitae varius augue, eu consectetur ligula. Etiam dui eros, laoreet sit amet est vel, commodo venenatis eroserto.</p>
+							<h4>HAC Systems Commissioning</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+							<a class="button request_services">Request Service</a>
 						</article>
 					</div>
-					<div class="grid_4">
-						<div class="img_block"><a href="images/index-1_img-5-big.jpg"><img src="images/index-1_img-5.jpg" alt="Services"><i>+</i></a></div>
-						<article>
-							<h4>Ferlima Nestro</h4>
-							<p>Gamus at magna non nunc tristique rhoncuseri tym. Aliquam nibh ante, egestas id dictum aterert commodo re luctus libero. Praesent faucibus malesuada cibuste. Donec laoreet metus id laoreet malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur orci sed Curabitur vel lorem sit amet nulla ullamcorper fermentum. In vitae varius augue, eu consectetur ligula. Etiam dui eros, laoreet sit amet est vel, commodo venenatis eros.</p>
-						</article>
-					</div>
-					<div class="grid_4">
-						<div class="img_block"><a href="images/index-1_img-6-big.jpg"><img src="images/index-1_img-6.jpg" alt="Services"><i>+</i></a></div>
-						<article>
-							<h4>Werdo Dasr</h4>
-							<p>Lamus at magna non nunc tristique rhoncuseri tym. Aliquam nibh ante, egestas id dictum aterert commodo re luctus libero. Praesent faucibus malesuada cibuste. Donec laoreet metus id laoreet malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur orci sed Curabitur vel lorem sit amet nulla ullamcorper fermentum. In vitae varius augue, eu consectetur ligula. Etiam dui eros, laoreet sit amet est vel, commodo venenatis eroso.</p>
-						</article>
-					</div>
-					<div class="grid_4">
-						<div class="img_block"><a href="images/index-1_img-7-big.jpg"><img src="images/index-1_img-7.jpg" alt="Services"><i>+</i></a></div>
-						<article>
-							<h4>Daslom Gerto</h4>
-							<p>Wamus at magna non nunc tristique rhoncuseri tym. Aliquam nibh ante, egestas id dictum aterert commodo re luctus libero. Praesent faucibus malesuada cibuste. Donec laoreet metus id laoreet malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur orci sed Curabitur vel lorem sit amet nulla ullamcorper fermentum. In vitae varius augue, eu consectetur ligula. Etiam dui eros, laoreet sit amet est vel, commodo venenatis eroser.</p>
-						</article>
-					</div>
-					<div class="grid_4">
-						<div class="img_block"><a href="images/index-1_img-8-big.jpg"><img src="images/index-1_img-8.jpg" alt="Services"><i>+</i></a></div>
-						<article>
-							<h4>Liro Dast</h4>
-							<p>Kamus at magna non nunc tristique rhoncuseri tym. Aliquam nibh ante, egestas id dictum aterert commodo re luctus libero. Praesent faucibus malesuada cibuste. Donec laoreet metus id laoreet malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur orci sed Curabitur vel lorem sit amet nulla ullamcorper fermentum. In vitae varius augue, eu consectetur ligula. Etiam dui eros, laoreet sit amet est vel, commodo venenatis erose.</p>
-						</article>
-					</div>
-					<div class="grid_4">
-						<div class="img_block"><a href="images/index-1_img-9-big.jpg"><img src="images/index-1_img-9.jpg" alt="Services"><i>+</i></a></div>
-						<article>
-							<h4>Severty Opor</h4>
-							<p>Gamus at magna non nunc tristique rhoncuseri tym. Aliquam nibh ante, egestas id dictum aterert commodo re luctus libero. Praesent faucibus malesuada cibuste. Donec laoreet metus id laoreet malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur orci sed Curabitur vel lorem sit amet nulla ullamcorper fermentum. In vitae varius augue, eu consectetur ligula. Etiam dui eros, laoreet sit amet est vel, commodo venenatis eros.</p>
-						</article>
-					</div>
+					
 				</div>
 			</div>
 		</div>
 
 	</div>
 </section>
+<style>
+a.button.request_services {
+    text-align: center;
+    display: block;
+    font-size: 24px;
+    background-color: #d94d3a;
+    margin: 20px;
+    padding: 20px;
+    color: white;
+    text-transform: uppercase;
+    cursor: pointer;
+}
+</style>
 
 <?php include("./footer.php");?>
