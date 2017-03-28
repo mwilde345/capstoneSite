9cc05032306daf5966e3dca75d7e1a8840f0e4df
Nathan Coats <nathangcoats@gmail.com>
2017-03-28 09:12:29 -0600

    adding project images, removing unused projects from the project page, changing image to open in new tab, removing pagination links at the bottom.

--- site/projects.php
+++ site/projects.php
@@ -19,72 +19,38 @@ jQuery("body").attr('class','index-2');
 				</div>
 				<div id="touch_gallery">
 					<div class="grid_4">
-						<div class="img_block"><a href="images/index-2_img-1-big.jpg"><img src="images/index-2_img-1.jpg" alt="Services"><i></i></a></div>
+						<div class="img_block"><a target="__blank" href="images/projects/byu_life_science_large.jpg"><img src="images/projects/byu_life_science.jpg" alt="Projects"><i></i></a></div>
 						<article>
-							<h4>Bliquam nibh ante</h4>
+							<h4>BYU Life Sciences Building</h4>
 							<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
 						</article>
 					</div>
 					<div class="grid_4">
-						<div class="img_block"><a href="images/index-2_img-2-big.jpg"><img src="images/index-2_img-2.jpg" alt="Services"><i></i></a></div>
+						<div class="img_block"><a target="__blank" href="images/projects/byu_auditorium_large.jpg"><img src="images/projects/byu_auditorium.jpg" alt="Projects"><i></i></a></div>
 						<article>
-							<h4>Aliquam nibh ante</h4>
+							<h4>BYU Idaho Auditorium</h4>
 							<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
 						</article>
 					</div>
 					<div class="grid_4">
-						<div class="img_block"><a href="images/index-2_img-3-big.jpg"><img src="images/index-2_img-3.jpg" alt="Services"><i></i></a></div>
+						<div class="img_block"><a target="__blank" href="images/projects/mortec_large.jpg"><img src="images/projects/mortec.jpg" alt="Projects"><i></i></a></div>
 						<article>
-							<h4>Cliqum nibh anteter</h4>
+							<h4>Mortec</h4>
 							<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
 						</article>
 					</div>
 					<div class="grid_4">
-						<div class="img_block"><a href="images/index-2_img-4-big.jpg"><img src="images/index-2_img-4.jpg" alt="Services"><i></i></a></div>
+						<div class="img_block"><a target="__blank" href="images/projects/century_link_large.jpg"><img src="images/projects/century_link.jpg" alt="Projects"><i></i></a></div>
 						<article>
-							<h4>Gyloiqum nibh ante</h4>
+							<h4>Century Link Telecom Systems</h4>
 							<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
 						</article>
 					</div>
-					<div class="grid_4">
-						<div class="img_block"><a href="images/index-2_img-5-big.jpg"><img src="images/index-2_img-5.jpg" alt="Services"><i></i></a></div>
-						<article>
-							<h4>Hqum nibh antetem</h4>
-							<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
-						</article>
-					</div>
-					<div class="grid_4">
-						<div class="img_block"><a href="images/index-2_img-6-big.jpg"><img src="images/index-2_img-6.jpg" alt="Services"><i></i></a></div>
-						<article>
-							<h4>Hoiqum nibh antetew</h4>
-							<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
-						</article>
-					</div>
-					<div class="grid_4">
-						<div class="img_block"><a href="images/index-2_img-7-big.jpg"><img src="images/index-2_img-7.jpg" alt="Services"><i></i></a></div>
-						<article>
-							<h4>Cliqum nibh anteter</h4>
-							<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
-						</article>
-					</div>
-					<div class="grid_4">
-						<div class="img_block"><a href="images/index-2_img-8-big.jpg"><img src="images/index-2_img-8.jpg" alt="Services"><i></i></a></div>
-						<article>
-							<h4>Moqum nibh untetek</h4>
-							<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
-						</article>
-					</div>
-					<div class="grid_4">
-						<div class="img_block"><a href="images/index-2_img-9-big.jpg"><img src="images/index-2_img-9.jpg" alt="Services"><i></i></a></div>
-						<article>
-							<h4>Dliqum nibh anteta</h4>
-							<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
-						</article>
-					</div>
+
 				</div>
-				<div class="grid_12">
+				<!-- <div class="grid_12">
 					<div class="pagination"><a href="#" class="active">1</a><a href="#">2</a></div>
-				</div>
+				</div> -->
 			</div>
 		</div>
 
