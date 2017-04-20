97de7e9703054860cda7cc6ca825ad07ecb96b53
Sara Topping <saramonster@users.noreply.github.com>
2017-04-20 16:20:41 -0600

    updated the styling on the form
    removed "how found" php variable since that was removed on form, updated the styling on the form

--- site/servicerequest.php
+++ site/servicerequest.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
-<?php
 
+<?php
 // Just making sure the fields are set.
 $messageSent = false;
 $nameErr     = "";
@@ -8,7 +8,6 @@ $companyErr  = "";
 $emailErr    = "";
 $messageErr  = "";
 $header2     = "";
-
 if (isset($_REQUEST["submit"])) {
 	$name=$_POST['name'];
 	$company=$_POST['company'];
@@ -16,20 +15,14 @@ if (isset($_REQUEST["submit"])) {
 	$phone = $_POST['phone'];
 	$message=$_POST['message'];
 	$subject = $_POST['subject'];
-
-	// this is just so there is no error message, i will leave for possible future use
-	$found   = "";
-
-	$formcontent="From: $name\nCompany: $company\nPhone: $phone\nSubject: $subject\nHow Found: $found\n\nMessage: \n$message";
+	
+	
+	$formcontent="From: $name\nCompany: $company\nPhone: $phone\nSubject: $subject\n\nMessage: \n$message";
 	$recipient = "nathangcoats@gmail.com";
 	$mailheader = "From: $email ";
-
-
-
 	if (!$name){
 		$nameErr = "Missing name";
 	}
-
 	if (!$company){
 		$companyErr = "Missing company name";
 	}
@@ -39,98 +32,77 @@ if (isset($_REQUEST["submit"])) {
 	if (!$message){
 		$messageErr = "Please type a message to send";
 	}
-
-
 	if (!$nameErr && !$companyErr && !$emailErr && !$messageErr){
 		mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 		echo "Thank You! Your message was sent.";
 		$messageSent = true;
 	}
-
-
-
 }
 ?>
-
+<head>
+<link rel="stylesheet" href="./style.css">  <!-- this line might be uncecessary leaving it just in case --> 
 <?php include("./header.php");?>
 <script type="text/javascript">
 jQuery("nav .sf-menu li[class='current']").attr('class','');
 jQuery("nav .sf-menu li").filter(":contains('Services')").attr('class','current');
 jQuery("body").attr('class','index-1');
 </script>
+
+</head>
 <!--=======content================================-->
-
+<body>
 <section id="content">
-	<link rel="stylesheet" type="text/css" href="./service-form.css">
-	<center> 
-<div class="container">
 
-	<?php
+
+
+
+<?php
 	if ( !$messageSent ) {
 		$header = "  <h1>Service Request Form:</h1>
 		Please fill out the following information and someone will contact you shortly: <br> <br>";
 		echo "$header";
-		?>
+	?>
 
 
-		<form class="form-horizontal" action="servicerequest.php" method="post">
-			<div class="form-group">
-				<label class="col-sm-2 control-label">Name:*</label>
-				<div class="col-sm-10">
-					<input type="text" name="name" class="form-control" placeholder="First & Last Name">
-					<?php echo "<p class='text-danger'>$nameErr</p>";?>
-				</div>
-			</div>
-			<div class="form-group">
-				<label class="col-sm-2 control-label">Company Name:*</label>
-				<div class="col-sm-10">
-					<input type="text" name="company" class="form-control">
-					<?php echo "<p class='text-danger'>$companyErr</p>";?>
+<div class="container">
+	<form action="servicerequest.php" method="post">
+		<label>Name:*</label>
+		<input type="text" name="name" placeholder="First & Last Name">
+		<?php echo "<p class='text-danger'>$nameErr</p>";?>
 
-				</div>
-			</div>
-			<div class="form-group">
-				<label class="col-sm-2 control-label">Email:*</label>
-				<div class="col-sm-10">
-					<input type="email" name="email" class="form-control">
-					<?php echo "<p class='text-danger'>$emailErr</p>";?>
-				</div>
-			</div>
-			<div class="form-group">
-				<label class="col-sm-2 control-label">Phone:</label>
-				<div class="col-sm-10">
-					<input type="text" name="phone" class="form-control">
-				</div>
-			</div>
-			<div class="form-group">
-				<label class="col-sm-2 control-label">Subject:</label>
-				<div class="col-sm-10">
-					<select class="form-control" name="subject">
-						<option value="other" selected>Please Choose a Subject...</option>
-						<option value="Service Request">Service Request</option>
-						<option value="Other">Other</option>
-					</select>
-				</div>
-			</div>
-			<div class="form-group">
-				<label class="col-sm-2 control-label">Message:*</label>
-				<div class="col-sm-10">
-					<textarea class="form-control" name="message" rows="4"></textarea>
-					<?php echo "<p class='text-danger'>$messageErr</p>";?>
 
-				</div>
-			</div>
+		<label>Company Name:*</label>
+		<input type="text" name="company">
+		<?php echo "<p class='text-danger'>$companyErr</p>";?>
 
-			<div class="form-group">
-				<div class="col-sm-offset-2 col-sm-10">
-					<button id="submit" name="submit" type="submit" class="btn btn-default">Submit</button>
-					<br>* required fields <br><br>
-				</div>
-			</div>
-		</form>
-		</div>
-	</center>
-		<?php
+
+		<label>Email:*</label>
+		<input type="email" name="email">
+		<?php echo "<p class='text-danger'>$emailErr</p>";?>
+
+
+		<label>Phone:</label>
+		<input type="text" name="phone">
+
+		<label>Subject:</label>
+		<select name="subject">
+			<option value="other" selected>Please Choose a Subject...</option>
+			<option value="Service Request">Service Request</option>
+			<option value="Other">Other</option>
+		</select>
+
+
+		<label>Message:*</label>
+		<textarea name="message" rows="4"></textarea>
+		<?php echo "<p class='text-danger'>$messageErr</p>";?>
+
+		<button id="submit" name="submit" type="submit">Submit</button>
+		<br>* required fields <br><br>
+
+	</form>
+</div>
+
+	<?php
 	}
 	else {
 		$header2 = "<h1>Thank you! Your message was sent.</h1>";
@@ -139,3 +111,4 @@ jQuery("body").attr('class','index-1');
 	?>
 <?php include("./footer.php");?>
 </section>
+</body>
