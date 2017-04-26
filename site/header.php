6d1ea588a0c5b8a79353d655d31c2345e3dcf950
Nathan Coats <nathangcoats@gmail.com>
2017-04-26 05:54:46 -0600

    modifying the TABB image size, removing about us page link from header, adding content to the whyus page

--- site/header.php
+++ site/header.php
@@ -67,7 +67,7 @@
 							</li>
 							<li><a href="servicerequest.php">Services</a></li>
 							<li><a href="projects.php">Projects</a></li>
-							<li><a href="about.php">About Us</a></li>
+							<!-- <li><a href="about.php">About Us</a></li> -->
 							<li><a href="contact.php">Contact Us</a></li>
 							<li><a href="whyus.php">Why Choose Us?</a></li>
 						</ul>
--- site/whyus.php
+++ site/whyus.php
@@ -15,9 +15,129 @@ jQuery("body").attr('class','index-1');
 					<header>
 						<h2><span>Why Choose Us?</span></h2>
 					</header>
+					<p>We are awesome!</p>
+					<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
 				</div>
-				
+			</div>
+			<br />
+			<br />
+			<div class="row">
+				<div class="grid_12">
+					<header>
+						<h4><span>Our Credentials</span></h4>
+					</header>
+				</div>
 
+				<div id="touch_gallery">
+					<div class="grid_4">
+						<a href="https://www.tabbcertified.org/site/public/content/index/home" style="display:block; min-height:210px;" target="_blank">
+							<img src="images/logos/TABB.png" width="100%" alt="TABB">
+						</a>
+						<article>
+							<h4>TABB Certified</h4>
+							<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
+						</article>
+					</div>
+					<div class="grid_4">
+						<a href="http://utahsheetmetal.com/" style="display:block; min-height:210px;text-align:center;"  target="__blank" >
+							<img src="images/logos/SMWIA_Logo.png" width="50%" alt="SMWIA">
+						</a>
+						<article>
+							<h4>SMIWIA 312 Members</h4>
+							<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
+						</article>
+					</div>
+					<div class="grid_4">
+						<a href="https://www.smacna.org/" style="display:block; min-height:210px;" target="__blank">
+							<img src="images/logos/SMACNA_Logo.png" width="100%" alt="SMACNA">
+						</a>
+						<article>
+							<h4>SMACNA Members</h4>
+							<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
+						</article>
+					</div>
+
+				</div>
+			</div>
+			<div class="row">
+				<div class="grid_12">
+					<header>
+						<h2><span>Who Are We?</span></h2>
+					</header>
+					<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
+				</div>
+			</div>
+			<br />
+			<br />
+			<div class="row">
+				<div class="grid_12">
+					<header>
+						<h4><span>Our Employees</span></h4>
+					</header>
+					<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
+				</div>
+
+				<div id="touch_gallery">
+					<div class="grid_4">
+						<a href="#employee_page" style="display:block; text-align:center;">
+							<img src="https://placeholdit.imgix.net/~text?txtsize=33&w=500&h=650&txt=Employee" width="75%" alt="Employee Image">
+						</a>
+						<article>
+							<h4>Jack Johnson</h4>
+							<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
+						</article>
+					</div>
+					<div class="grid_4">
+						<a href="#employee_page" style="display:block; text-align:center;" >
+							<img src="https://placeholdit.imgix.net/~text?txtsize=33&w=500&h=650&txt=Employee" width="75%" alt="Employee Image">
+						</a>
+						<article>
+							<h4>Bob Hope</h4>
+							<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
+						</article>
+					</div>
+					<div class="grid_4">
+						<a href="#employee_page" style="display:block; text-align:center;">
+							<img src="https://placeholdit.imgix.net/~text?txtsize=33&w=500&h=650&txt=Employee" width="75%" alt="Employee Image">
+						</a>
+						<article>
+							<h4>Bing Crosby</h4>
+							<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
+						</article>
+					</div>
+
+				</div>
+
+				<div id="touch_gallery">
+					<div class="grid_4">
+						<a href="#employee_page" style="display:block; text-align:center;">
+							<img src="https://placeholdit.imgix.net/~text?txtsize=33&w=500&h=650&txt=Employee" width="75%" alt="Employee Image">
+						</a>
+						<article>
+							<h4>Joe Blow</h4>
+							<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
+						</article>
+					</div>
+					<div class="grid_4">
+						<a href="#employee_page" style="display:block; text-align:center;" >
+							<img src="https://placeholdit.imgix.net/~text?txtsize=33&w=500&h=650&txt=Employee" width="75%" alt="Employee Image">
+						</a>
+						<article>
+							<h4>Josh Rogers</h4>
+							<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
+						</article>
+					</div>
+					<div class="grid_4">
+						<a href="#employee_page" style="display:block; text-align:center;">
+							<img src="https://placeholdit.imgix.net/~text?txtsize=33&w=500&h=650&txt=Employee" width="75%" alt="Employee Image">
+						</a>
+						<article>
+							<h4>Bill Cosby</h4>
+							<p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis erat mattis neque facilisis, sit amet ultricies erat rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh odio ut</p>
+						</article>
+					</div>
+
+				</div>
 				
 			</div>
 		</div>
